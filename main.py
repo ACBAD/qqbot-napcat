daec6490bc1fe63a9be13c8ffc6093f271717e7a
xd <2080776216@qq.com>
2025-12-11 21:30:32 +0800

    update checker online

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from ncatbot.core import BotClient
 from ncatbot.plugin_system import on_group_at
 from ncatbot.core.event import GroupMessageEvent
 bot = BotClient()
+WHAT_JPG = Path('what.jpg')
 
 
 @on_group_at
 async def at_func(event: GroupMessageEvent):
-    await event.reply(image=str(Path('what.jpg')))
+    if WHAT_JPG.exists():
+        await event.reply(image=str(WHAT_JPG))
 
 
 bot.run_frontend()
