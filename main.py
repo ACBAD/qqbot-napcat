a46623e077531a4951c90f04e29f4cc545f97cd9
xd <2080776216@qq.com>
2025-12-15 22:56:15 +0800

    base

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 from ncatbot.core import BotClient
-from ncatbot.plugin_system import on_group_at
-from ncatbot.core.event import GroupMessageEvent
+from ncatbot.plugin_system import on_group_poke
+from ncatbot.core.event import RequestEvent
+from ncatbot.utils import status
 bot = BotClient()
 WHAT_JPG = Path('what.jpg')
 
 
-@on_group_at
-async def at_func(event: GroupMessageEvent):
+@on_group_poke
+async def at_func(event: RequestEvent):
     if WHAT_JPG.exists():
-        await event.reply(image=str(WHAT_JPG))
+        await status.global_api.send_group_image(event.group_id, str(WHAT_JPG))
 
 
 bot.run_frontend()
