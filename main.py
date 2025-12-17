528945d107dead90b2cb6afd0b498d6eead81070
xd <2080776216@qq.com>
2025-12-17 16:30:04 +0800

    fix pixiv; fix poke

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from pathlib import Path
 from ncatbot.core import BotClient
 from ncatbot.plugin_system import on_group_poke
-from ncatbot.core.event import NoticeEvent, Poke
-from ncatbot.utils import status
+from ncatbot.core.event import PokeNoticeEvent
+from ncatbot.utils import status, get_log, ncatbot_config
 bot = BotClient()
 WHAT_JPG = Path('what.jpg')
+logger = get_log('Main')
 
 
-# @on_group_poke
-# async def at_func(event: NoticeEvent):
-#     status.global_api.get_msg(event.message_id)
-#     if WHAT_JPG.exists():
-#         await status.global_api.send_group_image(event.group_id, str(WHAT_JPG))
+@on_group_poke
+async def poke_func(event: PokeNoticeEvent):
+    if event.target_id != ncatbot_config.bt_uin:
+        return
+    if WHAT_JPG.exists():
+        await status.global_api.send_group_image(event.group_id, str(WHAT_JPG))
 
 
 bot.run_frontend()
